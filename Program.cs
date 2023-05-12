c3a10a5faffa3ffd4952ebc7e1059487d828ee6e
Mc Isti <90382294+mcisti@users.noreply.github.com>
2023-05-12 08:25:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MySqlConnector;
+
+namespace mysql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionstring;
+            MySqlConnection kapcsolat;
+            connectionstring = @"server = localhost; user=root;database=proba";
+            kapcsolat = new MySqlConnection(connectionstring);
+            kapcsolat.Open();
+            string sql = "SELECT * FROM szemely";
+            MySqlCommand mSqlCmd = new MySqlCommand(sql,kapcsolat);
+            MySqlDataReader adatok = mSqlCmd.ExecuteReader();
+            while (adatok.Read())
+            {
+                Console.WriteLine(adatok[1]);
+            }
+            kapcsolat.Close();
+        }
+    }
+}
